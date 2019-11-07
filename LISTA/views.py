b4386800ac2c938d2aab1a0fc6fcdf4bef9b6b04
fede <fedevargas110@gmail.com>
2019-11-07 14:00:13 -0300

    deleted imports in view

--- LISTA/views.py
+++ LISTA/views.py
@@ -2,15 +2,9 @@ from django.shortcuts import render, redirect
 from LISTA.models import Guest
 from LISTA.serializers import GuestSerializer
 from rest_framework import viewsets, permissions, status
-from rest_framework.decorators import action, api_view
-from rest_framework.response import Response
 from rest_framework import filters
-#from rest_framework.permissions import IsAuthenticated
-#from rest_framework.authentication import TokenAuthentication, SessionAuthentication
 
 class GuestViewSet(viewsets.ModelViewSet):
-#  permission_classes = (IsAuthenticated,)
-#  authentication_classes = (TokenAuthentication, SessionAuthentication)
   queryset = Guest.objects.all()
   serializer_class = GuestSerializer
   
